ee4f3199eec063d5c275bbb25644d18cec101fa8
Yuzhe Sheng <ys766@cornell.edu>
2017-11-28 23:49:12 -0500

    woodbury_inversion

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import scipy
 
 class model():
     def __init__(self,
@@ -20,6 +21,8 @@ class model():
         self.Kuser = self.__kernel__(a1,b1,self.user_feature)
         self.Kmovie = self.__kernel__(a2,b2,self.movie_feature)
         self.__distance_measure__()
+        self.__build_R__()
+        self.choose_inducing_point(0.1)
     @staticmethod
     def __load_data__(path):
         data = []
@@ -47,6 +50,13 @@ class model():
         self.user_distance = np.squeeze(np.sum(0.5*np.abs(user_feature1 - user_feature2),axis = 1))
         self.movie_distance = np.squeeze(np.sum(0.5*np.abs(movie_feature1 - movie_feature2),axis = 1))
 
+    def __build_R__(self):
+        self.R = -1*np.ones([len(self.user_feature),len(self.movie_feature)])
+        for i in self.indexed_training_set:
+            self.R[i[0]-1,i[1]-1] = i[2]
+        
+        
+
     def choose_inducing_point(self,ratio,pattern = 'random',source = 'training set'):
         # function used to choose inducing point from self.training_set
         # ratio: inducing_point/data_size
@@ -56,7 +66,7 @@ class model():
             m = np.ceil(n*ratio).astype(int)
             self.indexed_inducing_set = self.indexed_training_set[np.random.choice(n,m,replace = False)]
 
-    def build_Kuu(self,ratio,pattern = 'random'):
+    def build_Kuu(self):
         # hyp = [a_user,b_user,a_movie,b_movie]
         m = self.indexed_inducing_set.shape[0] # size of the inducing point set
         self.Kuu = np.matrix(np.ndarray([m,m],dtype = np.float32))
@@ -64,7 +74,7 @@ class model():
             for j in range(m):
                 user_i,movie_i,*rest = self.indexed_inducing_set[i]
                 user_j,movie_j,*rest = self.indexed_inducing_set[j]
-                self.Kuu[i,j] = self.Kuser[user_i,user_j]*self.Kmovie[movie_i,movie_j]
+                self.Kuu[i,j] = self.Kuser[user_i-1,user_j-1]*self.Kmovie[movie_i-1,movie_j-1]
     
             
     def build_sparse_W(self,pattern = 'train'):
@@ -77,12 +87,12 @@ class model():
         elif pattern == 'test':
             X = self.indexed_testing_set
         U = self.indexed_inducing_set
-        m = len(X)
-        n = len(U)
+        n = len(X)
+        m = len(U)
         W = np.matrix(np.zeros([m,n]))
-        for i in range(m):
+        for i in range(n):
             low_1,low_2,index_1,index_2 = [47,47,-1,-1]
-            for j in range(n):
+            for j in range(m):
                 ui,uj = X[i][0],U[j][0]
                 vi,vj = X[i][1],U[j][1]
                 buffer = self.user_distance[ui-1,uj-1] + self.movie_distance[vi-1,vj-1]
@@ -103,8 +113,8 @@ class model():
             self.testing_W = W
 
     def Linear_Conjugate_Gradient(self):
-        #Given W,Kuu out put the inverse of WKuuWT
-        self.inverse_WKuuWT
+        #Given W and Kuu, return inv(WKuuWT)*y
+        self.alpha = scipy.sparse.linalg.cg(self.Kuu,self.indexed_inducing_set[:,2])
         pass
 
     def Approximate_log_determinant(self):
@@ -112,9 +122,7 @@ class model():
         pass
 
     def predict(self):
-        self.mean = (self.training_W*self.Kuu.T*self.testing_W.T) * self.inverse_WKuuWT * np.matrix(self.indexed_training_set[:,2]) # K(x,x*) * inv(W*Kuu*WT) * y
-        self.variance = (self.testing_W*self.Kuu.T*self.testing_W.T) + (self.training_W*self.Kuu.T*self.testing_W.T) * self.inverse_WKuuWT * (self.testing_W*self.Kuu.T*self.training_W.T)
-        return [self.mean,self.variance]
+        self.mean = self.testing_W*self.Kuu*self.alpha
 
     def optimize_hyp(self):
         pass
